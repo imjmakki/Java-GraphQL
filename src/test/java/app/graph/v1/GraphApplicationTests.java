6cc7481e14220cb81bdda5d2d23c4578ea3dc42d
Mohamad J. Makki <malanbaki@transforat.com>
2022-05-05 09:59:34 +0300

    GraphQL

---
+++ src/test/java/app/graph/v1/GraphApplicationTests.java
@@ -0,0 +1,13 @@
+package app.graph.v1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraphApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
